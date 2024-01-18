a83b0730547a9c3ffb0ef811fc4f72b35f5ed31a
chrismoroney <christopher.moroney1@gmail.com>
2024-01-17 17:32:40 -0800

    initial pulls and counter

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import random as rand
+
+def ten_pull():
+    [one_pull() for _ in range(10)]
+
+def one_pull():
+    global counter
+    counter += 1
+    print(counter)
+
 if __name__ == '__main__':
-    print("Hello World!")
+    counter = 0
+
+    inp = input("enter command: ")
+    while(inp != 'q'):
+        if inp == '1':
+            one_pull()
+        if inp == '10':
+            ten_pull()
+        inp = input("enter command: ")
