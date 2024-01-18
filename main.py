82223985c42f60d99dc4af0aadb0a4b13edf61e4
chrismoroney <christopher.moroney1@gmail.com>
2024-01-17 20:15:12 -0800

    created gacha unweighted

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import random as rand
 
+def generate_five_star_occ():
+    return rand.randrange(0,100) + 1
+
+def generate_four_star_occ():
+    return rand.randrange(0,10) + 1
+
 def ten_pull():
     [one_pull() for _ in range(10)]
 
 def one_pull():
-    global counter
+    global counter, five_star, four_star
     counter += 1
-    print(counter)
+    if counter == four_star:
+        print('4-star item')
+        four_star = counter + generate_four_star_occ()
+    elif counter == five_star:
+        print('5-star item')
+        five_star = counter + generate_five_star_occ()
+    else:
+        print('3-star item')
+
+five_star = generate_five_star_occ()
+four_star = generate_four_star_occ()
 
 if __name__ == '__main__':
     counter = 0
-
+    five_star = generate_five_star_occ()
+    four_star = generate_four_star_occ()
+    print(five_star)
+    print(four_star)
     inp = input("enter command: ")
     while(inp != 'q'):
         if inp == '1':
