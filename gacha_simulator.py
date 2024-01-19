f13960128acc1eb8f9abe6f90990c7318aabe974
chrismoroney <christopher.moroney1@gmail.com>
2024-01-18 18:02:53 -0800

    weighted gacha system

--- gacha_simulator.py
+++ gacha_simulator.py
@@ -2,27 +2,31 @@ import random as rand
 
 class gacha_simulator():
     def __init__(self):
-        self.counter = 0
-        self.five_star = self.generate_five_star_occ()
-        self.four_star = self.generate_four_star_occ()
-
-    def generate_five_star_occ(self):
-        return rand.randrange(0,100) + 1
-
-    def generate_four_star_occ(self):
-        return rand.randrange(0,10) + 1
+        self.counter_four_star = 0
+        self.counter_five_star = 0
 
     def ten_pull(self):
         for _ in range(10):
             self.one_pull()
 
     def one_pull(self):
-        self.counter += 1
-        if self.counter == self.four_star:
-            print('4-star item')
-            self.four_star = self.counter + self.generate_four_star_occ()
-        elif self.counter == self.five_star:
+        pull = rand.randint(0, 99)
+        '''
+        four_star: 10% chance + 10% per pull after 10 pulls
+        five_star: 1% chance + 5% per pull after 100 pulls
+        '''
+        four_star_pull = 10 + 10 * max(0, self.counter_four_star - 10)
+        five_star_pull = 1 + 5 * max(0, self.counter_five_star - 100)
+
+        if pull < five_star_pull:
             print('5-star item')
-            self.five_star = self.counter + self.generate_five_star_occ()
+            self.counter_five_star = 0
+            self.counter_four_star = 0
         else:
-            print('3-star item')
+            if pull < four_star_pull:
+                print('4-star item')
+                self.counter_four_star = 0
+            else:
+                print('3-star item')
+            self.counter_five_star += 1
+            self.counter_four_star += 1
